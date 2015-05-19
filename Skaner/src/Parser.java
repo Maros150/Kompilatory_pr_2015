e6233afada1bcc46980cffeafb445e84f8994ee4
Maros150 <wbachta150@gmail.com>
2015-05-19 21:06:01 +0200

    Parser-Skoczek

--- Skaner/src/Parser.java
+++ Skaner/src/Parser.java
@@ -40,9 +40,9 @@ public class Parser {
 			else if(aktualny.token==Tokens.Pion){
 				parsePion();
 			}
-			/*else if(aktualny.equals(Tokens.Skoczek)){
+			else if(aktualny.equals(Tokens.Skoczek)){
 				parseSkoczek();
-			}*/
+			}
 			else
 				throw new Exception("Wrong token: oczekiwana figura-jest: "+ aktualny.znak+" "+aktualny.token+" ");
 			
@@ -88,35 +88,33 @@ public class Parser {
 		else
 			throw new Exception("Pion: wrong color token"+aktualny.znak);
 	}	
-			
-	/*
-	private void parseGoniec() throws Exception{
-		ruch.wspx_z=getToken(ruch,Tokens.WspX);
-		ruch.wspy_z=getToken(ruch,Tokens.WspY);
-		ruch.wspx_do=getToken(ruch,Tokens.WspX);
-		ruch.wspy_do=getToken(ruch,Tokens.WspY);
-		
-		if(ruch.wspy_z==ruch.wspy_do-2 && (ruch.wspx_z!=ruch.wspx_do+1 && ruch.wspx_z!=ruch.wspx_do-1))
-			throw new Exception("Goniec: zly ruch");
-			
-	}
-	private void parseHetman() throws Exception{
-		ruch.wspx_z=getToken(ruch,Tokens.WspX);
-		ruch.wspx_do=getToken(ruch,Tokens.WspX);
-		ruch.wspy_z=getToken(ruch,Tokens.WspY);
-		ruch.wspy_do=getToken(ruch,Tokens.WspY);
-	}
 
+	/*private void parseGoniec() throws Exception{
+			
+	}*/
+	/*private void parseHetman() throws Exception{
+		ruch.wspx_z=getToken(ruch,Tokens.WspX);
+		ruch.wspx_do=getToken(ruch,Tokens.WspX);
+		ruch.wspy_z=getToken(ruch,Tokens.WspY);
+		ruch.wspy_do=getToken(ruch,Tokens.WspY);
+	}
+*/
 	private void parseSkoczek() throws Exception{
-		ruch.wspx_z=getToken(ruch,Tokens.WspX);
-		ruch.wspx_do=getToken(ruch,Tokens.WspX);
-		ruch.wspy_z=getToken(ruch,Tokens.WspY);
-		ruch.wspy_do=getToken(ruch,Tokens.WspY);
+		aktualny = getToken(Tokens.WspX);	//X
+		switch(aktualny.znak){
+			case 'a':As();break;
+			case 'b':Bs();break;
+			case 'c':Cs();break;
+			case 'd':Ds();break;
+			case 'e':Es();break;
+			case 'f':Fs();break;
+			case 'g':Gs();break;
+			case 'h':Hs();break;
+			default	:
+				throw new Exception("Skoczek: wrong first token"+aktualny.toString());
+		}
 	}
-	
-	*/
 
-	
 	
 	
 	private void C() throws Exception{
@@ -262,6 +260,241 @@ public class Parser {
 		
 		if(aktualny.znak!=lookahead.znak+1)
 			throw new Exception("Q4:Wrong tokens expected "+ Tokens.WspY +", was: "+aktualny.token+ " "+lookahead.token);
+	}
+	
+	private void As() throws Exception{
+		if(findahead(1, Tokens.WspX).znak=='c'){
+			As1();		//c
+			Q7();
+		}
+		else{
+			Ax();
+			Q8();
+		}
+	}
+	private void Bs() throws Exception{
+		if(findahead(1, Tokens.WspX).znak=='d'){
+			Bs1();		//d
+			Q7();
+		}
+		else{
+			Bx();
+			Q8();
+		}	
+	}
+	private void Cs() throws Exception{
+		if(findahead(1, Tokens.WspX).znak=='e' || findahead(1, Tokens.WspX).znak=='e'){
+			Cs1();		//e lub f
+			Q7();
+		}
+		else{
+			Cx();
+			Q8();
+		}	
+	}
+	private void Ds() throws Exception{
+		if(findahead(1, Tokens.WspX).znak=='b' || findahead(1, Tokens.WspX).znak=='f'){
+			Ds1();		//b lub f
+			Q7();
+		}
+		else{
+			Dx();
+			Q8();
+		}	
+	}
+	private void Es() throws Exception{
+		if(findahead(1, Tokens.WspX).znak=='c' || findahead(1, Tokens.WspX).znak=='g'){
+			Es1();		//c lub g
+			Q7();
+		}
+		else{
+			Ex();
+			Q8();
+		}	
+	}
+	private void Fs() throws Exception{
+		if(findahead(1, Tokens.WspX).znak=='d' || findahead(1, Tokens.WspX).znak=='h'){
+			Fs1();		//d lub h
+			Q7();
+		}
+		else{
+			Fx();
+			Q8();
+		}	
+	}
+	private void Gs() throws Exception{
+		if(findahead(1, Tokens.WspX).znak=='e'){
+			Gs1();		//e
+			Q7();
+		}
+		else{
+			Gx();
+			Q8();
+		}		
+	}
+	private void Hs() throws Exception{
+		if(findahead(1, Tokens.WspX).znak=='f'){
+			Hs1();		//f
+			Q7();
+		}
+		else{
+			Hx();
+			Q8();
+		}		
+	}
+	
+	private void As1() throws Exception{
+		aktualny=getToken(Tokens.WspX);	
+		if(aktualny.znak!='c')
+			throw new Exception("As1:Wrong tokens expected 'c', was: "+aktualny.znak);
+	}	
+	private void Bs1() throws Exception{
+		aktualny=getToken(Tokens.WspX);	
+		if(aktualny.znak!='d')
+			throw new Exception("Bs1:Wrong tokens expected 'd', was: "+aktualny.znak);
+	}
+	private void Cs1() throws Exception{
+		aktualny=getToken(Tokens.WspX);	
+		if(aktualny.znak!='a' && aktualny.znak!='e')
+			throw new Exception("Cs1:Wrong tokens expected 'a lub e', was: "+aktualny.znak);
+	}	
+	private void Ds1() throws Exception{
+		aktualny=getToken(Tokens.WspX);	
+		if(aktualny.znak!='b' && aktualny.znak!='f')
+			throw new Exception("Ds1:Wrong tokens expected 'b lub f', was: "+aktualny.znak);
+	}	
+	private void Es1() throws Exception{
+		aktualny=getToken(Tokens.WspX);	
+		if(aktualny.znak!='c' && aktualny.znak!='g')
+			throw new Exception("Es1:Wrong tokens expected 'c lub g', was: "+aktualny.znak);
+	}	
+	private void Fs1() throws Exception{
+		aktualny=getToken(Tokens.WspX);	
+		if(aktualny.znak!='d' && aktualny.znak!='h')
+			throw new Exception("Fs1:Wrong tokens expected 'd lub h', was: "+aktualny.znak);
+	}	
+	private void Gs1() throws Exception{
+		aktualny=getToken(Tokens.WspX);	
+		if(aktualny.znak!='e')
+			throw new Exception("Gs1:Wrong tokens expected 'e', was: "+aktualny.znak);
+	}	
+	private void Hs1() throws Exception{
+		aktualny=getToken(Tokens.WspX);	
+		if(aktualny.znak!='f')
+			throw new Exception("Hs1:Wrong tokens expected 'f', was: "+aktualny.znak);
+	}
+	
+	private void Q7() throws Exception{
+		aktualny=getToken(Tokens.WspY);	
+		switch(aktualny.znak){
+			case '1':Ls1();break;
+			case '2':Ls2();break;
+			case '3':Ls3();break;
+			case '4':Ls4();break;
+			case '5':Ls5();break;
+			case '6':Ls6();break;
+			case '7':Ls7();break;
+			case '8':Ls8();break;
+			default	:
+				throw new Exception("Q7:Wrong tokens expected WspY, was: "+aktualny.token);
+		}
+	}
+	private void Ls1() throws Exception{
+		aktualny=getToken(Tokens.WspY);	
+		if(aktualny.znak!='2')
+			throw new Exception("Ls1:Wrong tokens expected '2', was: "+aktualny.znak);
+	}
+	private void Ls2() throws Exception{
+		aktualny=getToken(Tokens.WspY);	
+		if(aktualny.znak!='1' && aktualny.znak!='3')
+			throw new Exception("Ls2:Wrong tokens expected '1 lub 3', was: "+aktualny.znak);
+	}
+	private void Ls3() throws Exception{
+		aktualny=getToken(Tokens.WspY);	
+		if(aktualny.znak!='2' && aktualny.znak!='4')
+			throw new Exception("Ls3:Wrong tokens expected '2 lub 4', was: "+aktualny.znak);
+	}
+	private void Ls4() throws Exception{
+		aktualny=getToken(Tokens.WspY);	
+		if(aktualny.znak!='3' && aktualny.znak!='5')
+			throw new Exception("Ls4:Wrong tokens expected '3 lub 5', was: "+aktualny.znak);
+	}
+	private void Ls5() throws Exception{
+		aktualny=getToken(Tokens.WspY);	
+		if(aktualny.znak!='4' && aktualny.znak!='6')
+			throw new Exception("Ls5:Wrong tokens expected '4 lub 6', was: "+aktualny.znak);
+	}
+	private void Ls6() throws Exception{
+		aktualny=getToken(Tokens.WspY);	
+		if(aktualny.znak!='5' && aktualny.znak!='7')
+			throw new Exception("Ls6:Wrong tokens expected '5 lub 7', was: "+aktualny.znak);
+	}
+	private void Ls7() throws Exception{
+		aktualny=getToken(Tokens.WspY);	
+		if(aktualny.znak!='6' && aktualny.znak!='8')
+			throw new Exception("Ls7:Wrong tokens expected '6 lub 8', was: "+aktualny.znak);
+	}
+	private void Ls8() throws Exception{
+		aktualny=getToken(Tokens.WspY);	
+		if(aktualny.znak!='7')
+			throw new Exception("Ls8:Wrong tokens expected '7', was: "+aktualny.znak);
+	}
+	
+	private void Q8() throws Exception{
+		aktualny=getToken(Tokens.WspY);	
+		switch(aktualny.znak){
+			case '1':£s1();break;
+			case '2':£s2();break;
+			case '3':£s3();break;
+			case '4':£s4();break;
+			case '5':£s5();break;
+			case '6':£s6();break;
+			case '7':£s7();break;
+			case '8':£s8();break;
+			default	:
+				throw new Exception("Q8:Wrong tokens expected WspY, was: "+aktualny.token);
+		}
+	}
+
+	private void £s1() throws Exception{
+		aktualny=getToken(Tokens.WspY);	
+		if(aktualny.znak!='3')
+			throw new Exception("£s1:Wrong tokens expected '3', was: "+aktualny.znak);
+	}
+	private void £s2() throws Exception{
+		aktualny=getToken(Tokens.WspY);	
+		if(aktualny.znak!='4')
+			throw new Exception("£s2:Wrong tokens expected '4', was: "+aktualny.znak);
+	}
+	private void £s3() throws Exception{
+		aktualny=getToken(Tokens.WspY);	
+		if(aktualny.znak!='1' && aktualny.znak!='5')
+			throw new Exception("£s3:Wrong tokens expected '1 lub 5', was: "+aktualny.znak);
+	}
+	private void £s4() throws Exception{
+		aktualny=getToken(Tokens.WspY);	
+		if(aktualny.znak!='2' && aktualny.znak!='6')
+			throw new Exception("£s4:Wrong tokens expected '2 lub 6', was: "+aktualny.znak);
+	}
+	private void £s5() throws Exception{
+		aktualny=getToken(Tokens.WspY);	
+		if(aktualny.znak!='3' && aktualny.znak!='7')
+			throw new Exception("£s5:Wrong tokens expected '3 lub 7', was: "+aktualny.znak);
+	}
+	private void £s6() throws Exception{
+		aktualny=getToken(Tokens.WspY);	
+		if(aktualny.znak!='4' && aktualny.znak!='8')
+			throw new Exception("£s6:Wrong tokens expected '4 lub 8', was: "+aktualny.znak);
+	}
+	private void £s7() throws Exception{
+		aktualny=getToken(Tokens.WspY);	
+		if(aktualny.znak!='5')
+			throw new Exception("£s7:Wrong tokens expected '5', was: "+aktualny.znak);
+	}
+	private void £s8() throws Exception{
+		aktualny=getToken(Tokens.WspY);	
+		if(aktualny.znak!='6')
+			throw new Exception("£s8:Wrong tokens expected '6', was: "+aktualny.znak);
 	}
 	
 	
